f5d1e77f5a6c5b936a540411ca739608269f5bb8
다선테크 주식회사 <dasuntech.inc@gmail.com>
2025-12-28 21:03:22 +0900

    Update code after build

--- sCUBE_RXN_Main.c
+++ sCUBE_RXN_Main.c
@@ -60,9 +60,9 @@ void main(void)
 	while(1)
 	{
 		// 메인 루프
-		// string_Tx("Hello TMS320F28335 !!");
-		// DELAY_US(100000);
-		Solenoid_Test_Sequence();
+		string_Tx("Hello TMS320F28335 !!");
+		DELAY_US(100000);
+		// Solenoid_Test_Sequence();
 
 	}
 }
